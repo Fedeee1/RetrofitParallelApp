704eac96e0f996f11a796ff4a1ead25f686a02b8
fedeee1 <federico.martinez@vasscompany.com>
2024-03-01 08:41:00 +0100

    RetrofitClient

---
+++ app/src/main/java/com/example/retrofitparallelapp/commons/Constants.kt
@@ -0,0 +1,2 @@
+package com.example.retrofitparallelapp.commons
+
---
+++ app/src/main/java/com/example/retrofitparallelapp/core/RetrofitClient.kt
@@ -0,0 +1,30 @@
+package com.example.retrofitparallelapp.core
+
+import com.example.retrofitparallelapp.BuildConfig.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    init {
+        val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
+        httpClient.interceptors().clear()
+
+        if (DEBUG) {
+            // Creamos un interceptor y le indicamos el log level a usar
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            httpClient.addInterceptor(logging)
+        }
+    }
+
+    fun makeRetrofitService(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}
